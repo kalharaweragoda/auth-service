b5a106dd3a2c1efb202937bf369ab29b8a3503b4
kalharaweragoda <kalharaweragoda@gmail.com>
2026-03-27 23:57:58 +0530

    register.ts create

--- auth-app/src/app/register/register.ts
+++ auth-app/src/app/register/register.ts
@@ -1,9 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../services/AuthService';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
-  templateUrl: './register.html',
-  styleUrl: './register.css',
+  imports: [FormsModule, CommonModule, RouterModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
 })
-export class Register {}
+export class RegisterComponent {
+  username = '';
+  password = '';
+  role = '';
+  error = '';
+  success = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit(form: any) {
+    if (form.invalid) return;
+    this.authService.register(this.username, this.password, this.role).subscribe({
+      next: (res) => {
+        this.success = 'Registration successful!Please log in.';
+        this.error = '';
+        this.router.navigate(['/login']);
+      },
+      error: () => {
+        this.error = 'Registration failed';
+        this.success = '';
+      },
+    });
+  }
+}
